f6ba8299ed62c6c1d0175898fe3f83cecf574733
Puneet Kakkar <puneetkakkar91@gmail.com>
2025-07-03 18:42:38 -0400

    chore: migrate nx to 15.9.7

--- apps/service-2/jest.config.ts
+++ apps/service-2/jest.config.ts
@@ -2,13 +2,14 @@
 export default {
   displayName: 'service-2',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/apps/service-2',
